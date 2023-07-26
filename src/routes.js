1f3da8d25faa307df1b04b3253b663f3b649df73
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2023-07-26 18:44:57 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+import { Icon } from "@chakra-ui/react";
+import {
+  MdBarChart,
+  MdPerson,
+  MdHome,
+  MdLock
+} from "react-icons/md";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/dataTables";
+
+// Auth Imports
+import SignInCentered from "views/auth/signIn";
+import SignUp from "views/auth/signUp";
+import Teachers from "views/admin/teachers";
+
+const token = localStorage.getItem('token')
+const role = Number(localStorage.getItem('role'))
+
+
+const routes = token ? role === 1 ? [
+  {
+    name: "O'qituvchilar",
+    layout: "/admin",
+    path: "/home",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Teachers,
+  },
+] : role === 2 ? [
+  {
+    name: "O'quvchilar",
+    layout: "/admin",
+    path: "/home",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Teachers,
+  },
+  {
+    name: "Kontestlar",
+    layout: "/admin",
+    path: "/contests",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Teachers,
+  },
+  {
+    name: "Test qo'shish",
+    layout: "/admin",
+    path: "/create-test",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Teachers,
+  },
+] : [
+  {
+    name: "Asosiy",
+    layout: "/admin",
+    path: "/home",
+    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    component: MainDashboard,
+  },
+  {
+    name: "Kontestlar",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+    path: "/contests",
+    component: DataTables,
+  },
+  {
+    name: "Profil",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Profile,
+  }
+] : [
+
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignInCentered,
+  },
+  {
+    name: "Sign Up",
+    layout: "/auth",
+    path: "/sign-up",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignUp,
+  }
+];
+
+export default routes;
---
+++ src/views/auth/signIn/index.jsx
@@ -0,0 +1,207 @@
+/* eslint-disable */
+/*!
+  _   _  ___  ____  ___ ________  _   _   _   _ ___   
+ | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
+ | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
+ |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
+ |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
+                                                                                                                                                                                                                                                                                                                                       
+=========================================================
+* Horizon UI - v1.1.0
+=========================================================
+
+* Product Page: https://www.horizon-ui.com/
+* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
+
+* Designed and Coded by Simmmple
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+import React, { useRef } from "react";
+import { NavLink } from "react-router-dom";
+// Chakra imports
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+// Custom components
+import { HSeparator } from "components/separator/Separator";
+import DefaultAuth from "layouts/auth/Default";
+// Assets
+import illustration from "assets/img/auth/auth.png";
+import { FcGoogle } from "react-icons/fc";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { RiEyeCloseLine } from "react-icons/ri";
+
+function SignIn() {
+  // Chakra color mode
+  const textColor = useColorModeValue("navy.700", "white");
+  const textColorSecondary = "gray.400";
+  const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
+  const textColorBrand = useColorModeValue("brand.500", "white");
+  const brandStars = useColorModeValue("brand.500", "brand.400");
+  const googleBg = useColorModeValue("secondaryGray.300", "whiteAlpha.200");
+  const googleText = useColorModeValue("navy.700", "white");
+  const googleHover = useColorModeValue(
+    { bg: "gray.200" },
+    { bg: "whiteAlpha.300" }
+  );
+  const googleActive = useColorModeValue(
+    { bg: "secondaryGray.300" },
+    { bg: "whiteAlpha.200" }
+  );
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+
+  const username = useRef()
+  const password = useRef()
+
+  if (localStorage.getItem('token')) {
+    location.replace('/')
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    localStorage.setItem('token', 'ahcsbakjcaics')
+    localStorage.setItem('role', 1)
+    localStorage.setItem('user', JSON.stringify({ username: username.current.value, password: password.current.value }))
+    location.replace('/')
+  }
+
+
+  return (
+    <DefaultAuth illustrationBackground={illustration} image={illustration}>
+      <Flex
+        maxW={{ base: "100%", md: "max-content" }}
+        w='100%'
+        mx={{ base: "auto", lg: "0px" }}
+        me='auto'
+        h='100%'
+        alignItems='start'
+        justifyContent='center'
+        mb={{ base: "30px", md: "60px" }}
+        px={{ base: "25px", md: "0px" }}
+        mt={{ base: "40px", md: "14vh" }}
+        flexDirection='column'>
+        <Box me='auto'>
+          <Heading color={textColor} fontSize='36px' mb='10px'>
+            Hisobga Kirish
+          </Heading>
+        </Box>
+        <Flex
+          zIndex='2'
+          direction='column'
+          w={{ base: "100%", md: "420px" }}
+          maxW='100%'
+          background='transparent'
+          borderRadius='15px'
+          mx={{ base: "auto", lg: "unset" }}
+          me='auto'
+          mb={{ base: "20px", md: "auto" }}>
+          <FormControl>
+            <form onSubmit={handleSubmit}>
+              <FormLabel
+                display='flex'
+                ms='4px'
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                mb='8px'>
+                Username<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <Input
+                isRequired={true}
+                variant='auth'
+                fontSize='sm'
+                ms={{ base: "0px", md: "0px" }}
+                type='text'
+                placeholder='@doniyorcoder'
+                mb='24px'
+                fontWeight='500'
+                size='lg'
+                ref={username}
+              />
+              <FormLabel
+                ms='4px'
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                display='flex'>
+                Password<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <InputGroup size='md'>
+                <Input
+                  isRequired={true}
+                  fontSize='sm'
+                  placeholder='Min. 8 ta belgi'
+                  mb='24px'
+                  size='lg'
+                  type={show ? "text" : "password"}
+                  variant='auth'
+                  ref={password}
+                />
+                <InputRightElement display='flex' alignItems='center' mt='4px'>
+                  <Icon
+                    color={textColorSecondary}
+                    _hover={{ cursor: "pointer" }}
+                    as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                    onClick={handleClick}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <Button
+                fontSize='sm'
+                variant='brand'
+                fontWeight='500'
+                w='100%'
+                h='50'
+                mb='24px'
+                type="submit"
+              >
+                Kirish
+              </Button>
+            </form>
+          </FormControl>
+          <Flex
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='start'
+            maxW='100%'
+            mt='0px'>
+            <Text color={textColorDetails} fontWeight='400' fontSize='14px'>
+              Hisobingiz yo'qmi?
+              <NavLink to='/auth/sign-up'>
+                <Text
+                  color={textColorBrand}
+                  as='span'
+                  ms='5px'
+                  fontWeight='500'>
+                  Ro'yxatdan o'ting
+                </Text>
+              </NavLink>
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </DefaultAuth>
+  );
+}
+
+export default SignIn;
